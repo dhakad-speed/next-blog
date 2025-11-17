3f0c8743cf437b4db7c39d7aa7e07a2c01b7f47a
dhakad-speed <dhakadtryspeed@gmail.com>
2025-11-17 15:30:47 +0530

    breakpoint

--- src/theme.ts
+++ src/theme.ts
@@ -5,15 +5,6 @@ const theme = createTheme({
   typography: {
     fontFamily: '"Inter", sans-serif',
   },
-  breakpoints: {
-    values: {
-      xs: 0,
-      sm: 480,
-      md: 768,
-      lg: 1024,
-      xl: 1280,
-    },
-  },
 });
 
 export default theme;
